1be233f94037d80c67cc63512677a25a976122cc
Giovanna L. <giovannagrafica@gmail.com>
2025-05-13 17:48:46 +0200

    fix: remove @import from CSS, import gallery.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'         // importa qui il tuo CSS TAILWIND
+
+// importa qui i CSS
+import './index.css'
+import './gallery.css'   // <— così il CSS della masonry diventa parte del bundle
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 )
+
 
 
 
